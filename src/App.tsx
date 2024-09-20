bc5dbf2046cb8c50fecae30732e1bf5640a7c798
HoKaHei001 <132826246+HoKaHei001@users.noreply.github.com>
2024-09-20 17:16:33 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,37 @@ import { useState, useEffect } from 'react'
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [albumData, setAlbumData] = useState<{ cid: string, name: string, coverUrl: string }[] | null>(null) // 型を修正
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      try {
+
+        const response = await fetch("http://localhost:3000/albums");
+        const result = await response.json();
+        setAlbumData(result.data);
+
+      } catch (error) {
+
+      }
+    }
+    fetchData();
+  }, [])
+
+  console.log(albumData);
 
   return (
     <>
-
+      {albumData && albumData.map((album: { cid: string, name: string, coverUrl: string }, index: number) => (
+        <div key={index}>
+          <p>{album.name}</p>
+          <p>{album.cid}</p>
+          <img src={album.coverUrl} alt="" />
+          <p>{album.coverUrl}</p>
+        </div>
+      ))}
     </>
   )
 }
